53b64abad98c46e93743a35c1370e7cd8cd8ab55
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-10 13:38:32 +0200

    added 404

---
+++ src/components/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class 404 extends React.Component {
+  render () {
+    return (
+      <h1>404 - Not Found</h1>
+      <h2>Ups... The page you're looking for doesn'e Exist...</h2>
+    )
+  }
+}
+
+export default 404;
