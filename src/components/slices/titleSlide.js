1f3aae0aa4b9877feceefeb0a74b1616596649ac
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-10 18:22:34 +0200

    created title slide

---
+++ src/components/slices/titleSlide.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from '../../assets/css/TitleSlide.module.scss';
+
+class TitleSlide extends React.Component {
+  render () {
+    return (
+      <section>
+        <span><h1>Tameto</h1><h1>Design</h1></span>
+        <span className={styles.slogan}>Web design and Development</span>
+      </section>
+    )
+  }
+}
