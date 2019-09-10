793f749ce5f658fc32d6e3e4757b117e50ac2127
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-10 19:04:54 +0200

    portfolio section

--- src/components/slices/portfolio.js
+++ src/components/slices/portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Title from '../modules/title';
+
 import data from '../../data/home.json';
 import styles from '../../assets/css/Portfolio.module.scss';
 import universal from '../../assets/css/Universal.module.scss';
@@ -9,14 +11,17 @@ class Portfolio extends React.Component {
     const projects = data.portfolio.projects;
 
     return (
-      <Title className={universal.yellow}>Portfolio</Title>
-      <section className={styles.projects}>
+      <section>
+        <Title className={universal.yellow}>Portfolio</Title>
+        <section className={styles.projects}>
 
-        {projects.map((project, index) => {
-          return (
-            <a href={project.link}><img src={project.logo} alt={project.name +  "logo"} key={project.link}/></a>
-          )
-        })}
+          {projects.map((project, index) => {
+            return (
+              <a href={project.link}><img src={project.logo} alt={project.name +  "logo"} key={project.link}/></a>
+            )
+          })}
+
+        </section>
       </section>
     )
   }
