1377dc5d6b350eeba1a35b425f605c9e7294a81d
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-10 19:21:53 +0200

    created skills section

---
+++ src/components/slices/skills.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Title from '../modules/title';
+
+import universal from '../../assets/css/Universal.module.scss';
+import styles from '../../assets/css/Skills.module.scss';
+import data from '../../data/home.json';
+
+
+class Skills extends React.Component {
+  render () {
+    const skills = data.skills.skillList;
+
+    return (
+      <section>
+        <Title className={universal.blue}>Skills</Title>
+        <section className={styles.skillList}>
+          {skills.map((skill, index) => {
+            return (
+              <img alt={skill.name + " logo"} key={skill.name} src={skill.icon}/>
+            )
+          })}
+        </section>
+      </section>
+    )
+  }
+}
+
+export default Skills;
