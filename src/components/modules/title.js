9d28197b6886b0afe0a5c05f5c72459badf2a6c0
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-11 13:31:59 +0200

    styled title slide

--- src/components/modules/title.js
+++ src/components/modules/title.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
+
 // eslint-disable-next-line
 import styles from '../../assets/css/Title.module.scss';
+import universal from '../../assets/css/Universal.module.scss';
 
 class Title extends React.Component {
   render () {
     const content = this.props.children;
 
     return (
-      <span><h1>{content.charAt(0)}</h1><h1>{content.substring(1)}</h1></span>
+      <span className={classnames(styles.title, universal.inline, universal.yellow)}><h1>{content.charAt(0)}</h1><h1 className={classnames(this.props.color === "yellow" ? universal.yellowOutline : universal.blue)}>{content.substring(1)}</h1></span>
     )
   }
 }
