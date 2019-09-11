6bfbe74451b0f74a86eb2b1388cac03404378c7c
Matteo Carpi <matteocarpi94@gmail.com>
2019-09-11 13:41:07 +0200

    fixed style of title

--- src/components/modules/title.js
+++ src/components/modules/title.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import classnames from 'classnames';
+import React from "react";
+import classnames from "classnames";
 
 // eslint-disable-next-line
-import styles from '../../assets/css/Title.module.scss';
-import universal from '../../assets/css/Universal.module.scss';
+import styles from "../../assets/css/Title.module.scss";
+import universal from "../../assets/css/Universal.module.scss";
 
 class Title extends React.Component {
-  render () {
+  render() {
     const content = this.props.children;
 
     return (
-      <span className={classnames(styles.title, universal.inline, universal.yellow)}><h1>{content.charAt(0)}</h1><h1 className={classnames(this.props.color === "yellow" ? universal.yellowOutline : universal.blue)}>{content.substring(1)}</h1></span>
-    )
+      <span
+        className={classnames(styles.title, universal.inline, universal.yellow)}
+      >
+        <h1
+          className={classnames(
+            this.props.color === "yellow" ? universal.yellow : universal.blue
+          )}
+        >
+          {content.charAt(0)}
+        </h1>
+        <h1
+          className={classnames(
+            this.props.color === "yellow"
+              ? universal.yellowOutline
+              : universal.blueOutline
+          )}
+        >
+          {content.substring(1)}
+        </h1>
+      </span>
+    );
   }
 }
 
