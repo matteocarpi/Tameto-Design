538d2bd57e39c9f8211e528f52f1973b6ad1a64a
Matteo Carpi <matteocarpi94@gmail.com>
2019-12-18 14:10:40 +0100

    fix: clean up header code

--- src/components/fundamentals/header.js
+++ src/components/fundamentals/header.js
@@ -18,16 +18,6 @@ class Header extends React.Component {
     };
   }
 
-  // listenScrollEvent = e => {
-  //   window.scrollY > 580
-  //     ? this.setState({ isPageScrolled: true })
-  //     : this.setState({ isPageScrolled: false });
-  // }
-
-  // componentDidMount() {
-  //   window.addEventListener('scroll', this.listenScrollEvent)
-  // }
-
   handleMenuButtonClick = () => {
     this.setState({ isMenuOpen: !this.state.isMenuOpen });
   };
@@ -41,35 +31,6 @@ class Header extends React.Component {
 
     return (
       <header>
-        {/* <nav className={styles.mobile}>
-          <div
-            className={styles.menuButton}
-            onClick={this.handleMenuButtonClick}
-          >
-            <i class="fas fa-bars" />
-          </div>
-          <ul
-            className={classnames(
-              styles.mobileMenu,
-              isMenuOpen ? styles.open : styles.close
-            )}
-          >
-            {navigation.map((item, index) => {
-              const firstLetter = item.label.charAt(0);
-              const restOfLabel = item.label.substring(1);
-
-              return (
-                <li>
-                  <Link onClick={this.closeMenu} smooth to={item.link}>
-                    <span className={universal.bold}>{firstLetter}</span>
-                    <span>{restOfLabel}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav> */}
-
         <nav className={styles.desktop}>
           <ul className={styles.desktopMenu}>
             {navigation.map((item, index) => {
